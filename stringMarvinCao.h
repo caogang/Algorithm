2b7e8f2c68251b6b0e84703caeb6371416ec765d
MarvinCao <caogang440@163.com>
2016-06-28 20:01:59 +0800

    finish the string rotate

--- stringMarvinCao.h
+++ stringMarvinCao.h
@@ -11,14 +11,45 @@
 #include <stdlib.h>
 
 /**
- * Description: Rotate the string.
+ * Description: Rotate the string using brute-force algorithm.
  * Input:
  *      str: string to be rotated
  *      n:   the length of the str
  *      m:   the first m characters would be rotate to the end of str
  * Output:
  *      str: string which has been rotated
+ * Return:
+ *      -1:  rotate error.
+ *      0:   rotate success.
  */
-void stringRotate(char* str, int n, int m);
+int stringRotateBrute(char* str, int n, int m);
+
+
+/**
+ * Description: Rotate the string using three-step-revert algorithm.
+ * Input:
+ *      str: string to be rotated
+ *      n:   the length of the str
+ *      m:   the first m characters would be rotate to the end of str
+ * Output:
+ *      str: string which has been rotated
+ * Return:
+ *      -1:  rotate error.
+ *      0:   rotate success.
+ */
+int stringRotateThreeStepRevert(char* str, int n, int m);
+
+/**
+ * Description: Rotate the string using three-step-revert algorithm.
+ * Input:
+ *      str: string to be rotated using space delimiter
+ *      n:   the length of the str
+ * Output:
+ *      str: string which has been rotated
+ * Return:
+ *      -1:  rotate error.
+ *      0:   rotate success.
+ */
+int stringRotateThreeStepRevertUsingSpaceDelimiter(char* str, int n);
 
 #endif /* STRINGMARVINCAO_H_ */
